b3d9490430e5255e92595a28e7318a1de22406b5
Darwin Fotso <136787768+darwinnatha@users.noreply.github.com>
2026-04-10 17:32:46 +0100

    Feat/deployment plan (#19)
    * fix: add missing exif php extension for composer dependencies
    
    Signed-off-by: Darwin Nathan <fotsodarwin@gmail.com>
    
    * Add Laravel Telescope and CORS config
    
    Signed-off-by: Darwin Nathan <fotsodarwin@gmail.com>
    
    ---------
    
    Signed-off-by: Darwin Nathan <fotsodarwin@gmail.com>

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
 use App\Providers\RepositoryServiceProvider;
+use App\Providers\TelescopeServiceProvider;
 
 return [
     AppServiceProvider::class,
     RepositoryServiceProvider::class,
+    TelescopeServiceProvider::class,
 ];
