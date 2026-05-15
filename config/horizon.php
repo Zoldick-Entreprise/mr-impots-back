cb3b4e7c12701ae8c1060242cf5bc976cdc0f2e5
Darwin Fotso <fotsodarwin@gmail.com>
2026-05-15 11:38:48 +0100

    Mark OCR pending and draft the document on upload and add Horizon (#47)
    staging
    
    Signed-off-by: Darwin Nathan <fotsodarwin@gmail.com>

--- config/horizon.php
+++ config/horizon.php
@@ -220,7 +220,13 @@ return [
                 'balanceCooldown' => 3,
             ],
         ],
-
+        'staging' => [
+            'supervisor-1' => [
+                'maxProcesses' => 3,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
         'local' => [
             'supervisor-1' => [
                 'maxProcesses' => 3,
